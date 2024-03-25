779f31578f7b17fbf8eacbb419b5536ee8355d0b
f-yusei <fkys2932@gmail.com>
2024-03-25 23:12:40 +0900

    chore: ciとかformatterの設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Router } from './router';
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router";
 
 function App() {
   return (
--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,3 @@
-export default function SignUp(){
-    return (
-        <h1>Hello SignUp</h1>
-    );
+export default function SignUp() {
+  return <h1>Hello SignUp</h1>;
 }
