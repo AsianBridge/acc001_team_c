f665654867c204f293d15dadba277208c98dece7
f-yusei <fkys2932@gmail.com>
2024-03-25 22:48:29 +0900

    feat: no style header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Router />
+      <Footer />
     </BrowserRouter>
   );
 }
