18234f4906acabe90f44a95f8d67f8e7354742c7
f-yusei <fkys2932@gmail.com>
2024-03-25 23:50:03 +0900

    chore: format

--- frontend/src/pages/MyAccount/MyAccount.tsx
+++ frontend/src/pages/MyAccount/MyAccount.tsx
@@ -1,12 +1,12 @@
 import { UserProfile } from "../../components/UserInformation";
 
 const MyAccount = () => {
-    return (
-        <>
-            <UserProfile />
-            <h3>Hello MyAccount</h3>
-        </>
-    );
-}
+  return (
+    <>
+      <UserProfile />
+      <h3>Hello MyAccount</h3>
+    </>
+  );
+};
 
 export default MyAccount;
