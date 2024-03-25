7245743c638132013bdd10e477ec3e66bfd28f9d
f-yusei <fkys2932@gmail.com>
2024-03-25 16:17:46 +0900

    feat: goの動作環境のairを設定

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+func say_hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	http.HandleFunc("/", say_hello)
+	http.ListenAndServe(":8081", nil)
 }
