a64a6bec7855d614725f285143878136a57f563a
akito-0520 <n.akito.h17@icloud.com>
2024-03-26 12:01:23 +0900

    add: 評価表示機能の作成

---
+++ frontend/src/features/StoreEvaluation.tsx
@@ -0,0 +1,16 @@
+import { Box, Rating, Typography } from "@mui/material";
+import { StoreEvaluationProps } from "../types";
+
+
+export const StoreEvaluation = ({ taste, atmosphere, costPerformance }: StoreEvaluationProps) => {
+    return (
+        <Box>
+            <Typography component="legend">味</Typography>
+            <Rating name="read-only" value={taste} readOnly />
+            <Typography component="legend">お店の雰囲気</Typography>
+            <Rating name="read-only" value={atmosphere} />
+            <Typography component="legend">コスパ</Typography>
+            <Rating name="read-only" value={costPerformance} />
+        </Box>
+    )
+}
