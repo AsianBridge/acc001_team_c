9ea6b6673768fbcf14348a7ecb3e16bd833e27cd
akito-0520 <n.akito.h17@icloud.com>
2024-03-26 11:02:54 +0900

    fix: コンフリクトの解決

--- frontend/src/components/ShowImage.tsx
+++ frontend/src/components/ShowImage.tsx
@@ -1,22 +1,30 @@
-export const ShowImage = (props:any) => {
-    const {
-        src,
-        ...other
-    } = props;
-    return (
-        <img src={src}
-            {...other}></img>
-    );
-}
+type Props = {
+  src: string;
+  width: string;
+  height: string;
+};
+
+export const ShowImage = (props: Props ) => {
+  const { src, ...other } = props;
+  return <img src={src} {...other}></img>;
+};
 
 export const HomeImage = () => {
-    return (
-        <ShowImage src="https://loosedrawing.com/assets/illustrations/png/487.png" width="50vw" height="50vh"/>
-    )
-}
+  return (
+    <ShowImage
+      src="https://loosedrawing.com/assets/illustrations/png/487.png"
+      width="50vw"
+      height="50vh"
+    />
+  );
+};
 
 export const AccountImage = () => {
-    return (
-        <ShowImage src="https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_13.png" width="50vw" height="50vh" />
-    )
-}
+  return (
+    <ShowImage
+      src="https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_13.png"
+      width="50vw"
+      height="50vh"
+    />
+  );
+};
