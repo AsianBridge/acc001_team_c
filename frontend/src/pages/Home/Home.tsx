e9fc10437fac142496fea4c40224765f439f7614
akito-0520 <n.akito.h17@icloud.com>
2024-03-26 11:09:19 +0900

    fix: import文の訂正

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,7 @@
-import { FooterMenu } from "../../components/ButtonAggregation";
-
 export default function Home() {
   return (
     <>
       <h3>Hello Home</h3>
-      <FooterMenu />
     </>
   );
 }
