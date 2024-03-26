86690a44cd7d6b5ee7652ba39e15fba820300651
khuvit <khuvia01@gmail.com>
2024-03-26 16:03:02 +0900

    Co-authored-by: Akito <akito-0520@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
+<<<<<<< Updated upstream
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+=======
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { Router } from './router';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Router />
+      
       <Footer />
     </BrowserRouter>
   );
