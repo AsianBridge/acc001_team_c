dafeb4b1617bfd1b0ead542b715f4578eb5a1b74
akito-0520 <n.akito.h17@icloud.com>
2024-03-26 10:34:40 +0900

    update: Modal機能の変更

--- frontend/src/components/ShowModal.tsx
+++ frontend/src/components/ShowModal.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Modal } from "@mui/material";
 import React from "react";
 import { ShowImage } from "./ShowImage";
 
-export const ShowModal = ({ src, storeName }: { src: string, storeName: string }) => {
+export const BingoSquareShowModal = ({ src, storeName }: { src: string | undefined, storeName: string }) => {
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
@@ -15,7 +15,11 @@ export const ShowModal = ({ src, storeName }: { src: string, storeName: string }
     return (
         <>
             <Button onClick={handleOpen} >
-                <ShowImage src={src} width="120vw" height="120vh" />
+                {src === undefined ?
+                    <h3>{storeName}へ行こう</h3>
+                    :
+                    <ShowImage src={src} width="120vw" height="120vh" />
+                }
             </Button>
             <Modal
                 open={open}
@@ -24,8 +28,16 @@ export const ShowModal = ({ src, storeName }: { src: string, storeName: string }
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={boxStyle}>
-                    <ShowImage src={src} width="200vw" height="200vh"></ShowImage>
-                    <h1>{storeName}</h1>
+                    {src === undefined ?
+                        <Box width="50vw" height="10vh">
+                            <h3>{storeName}へ行こう</h3>
+                        </Box>
+                        :
+                        <>
+                            <ShowImage src={src} width="200vw" height="200vh"></ShowImage>
+                            <h1>{storeName}</h1>
+                        </>
+                    }
                 </Box>
             </Modal>
         </>
--- frontend/src/features/Bingo.tsx
+++ frontend/src/features/Bingo.tsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import { ShowModal } from "../components/ShowModal";
+import { BingoSquareShowModal } from "../components/ShowModal";
 import { Grid } from "@mui/material";
+import { BingoSquareInformation } from "../types";
 
-const Square = ({ storeName, src, squareUpdate }: { storeName: string, src: string, squareUpdate: (value: number) => void }
-) => {
+const Square = ({
+    index,
+    storeName,
+    src,
+}: {
+    index: number,
+    storeName: string,
+    src: string | undefined,
+}) => {
     return (
-        <>
-            <ShowModal src={src} storeName={storeName} />
-        </>
+        <BingoSquareShowModal src={src} storeName={storeName} />
     );
 }
 
 const Bingo = () => {
-    const [bingoSquareState, setBingoSquareState] = useState<boolean[]>(Array(9).fill(false));
+    const [bingoSquareInformationState, setBingoSquareInformationState] = useState<BingoSquareInformation[]>(Array(9).fill({ storeName: undefined, src: undefined }));
+
+    const squareUpdate = (value: number, storeName: string, src: string) => {
+        const nextSquareInformationState = bingoSquareInformationState.slice();
+        nextSquareInformationState[value] = { storeName, src };
+
+        setBingoSquareInformationState(nextSquareInformationState);
+    }
 
     const storeInformation = [
         { storeName: "マック", src: "https://pbs.twimg.com/profile_images/1726395545974112256/3bTbEpwe_400x400.jpg" },
         { storeName: "一風堂", src: "https://ec-ippudo.com/img/usr/top/stores/pc/ippudo.jpg" },
-        { storeName: "築地銀だこ", src: "https://pbs.twimg.com/profile_images/1632537593777913857/v0yABIUT_400x400.jpg" },
+        { storeName: "築地銀だこ", src: undefined },
         { storeName: "築地銀だこ", src: "https://pbs.twimg.com/profile_images/1632537593777913857/v0yABIUT_400x400.jpg" },
         { storeName: "築地銀だこ", src: "https://pbs.twimg.com/profile_images/1632537593777913857/v0yABIUT_400x400.jpg" },
         { storeName: "築地銀だこ", src: "https://pbs.twimg.com/profile_images/1632537593777913857/v0yABIUT_400x400.jpg" },
@@ -26,19 +39,11 @@ const Bingo = () => {
         { storeName: "築地銀だこ", src: "https://pbs.twimg.com/profile_images/1632537593777913857/v0yABIUT_400x400.jpg" }
     ];
 
-
-    const squareUpdate = (value: number) => {
-        const nextSquareState = bingoSquareState.slice();
-        nextSquareState[value] = true;
-
-        setBingoSquareState(nextSquareState);
-    }
-
     return (
         <Grid container spacing={1}>
             {storeInformation.map((store, index) => (
                 <Grid item xs={4} key={index}>
-                    <Square storeName={store.storeName} src={store.src} squareUpdate={() => squareUpdate(index)} />
+                    <Square index={index} storeName={store.storeName} src={store.src} />
                 </Grid>
             ))}
         </Grid>
