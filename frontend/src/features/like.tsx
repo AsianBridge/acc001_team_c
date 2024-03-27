35a12b96fc52e5c637d246915c9c61bf3c94aa80
ginnabee6 <i211342@gm.ishikawa-nct.ac.jp>
2024-03-27 13:29:19 +0900

    add: いいね機能追加、ホームのデザイン変更

---
+++ frontend/src/features/like.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Like() {
+  return (
+    <div className="Like">
+      <header className="Like-header">
+        <LikeButton />
+      </header>
+    </div>
+  );
+}
+
+function LikeButton() {
+    const [color, setColor] = useState("white"); // 初期色を赤に設定
+  
+    const handleClick = () => {
+      // 色を切り替えるロジック
+      setColor(color === "white" ? "red" : "white");
+    };
+  
+    return (
+      <span className="likeButton" onClick={handleClick}>
+        <p
+        style={{
+          color: color,
+          fontSize: "2.0rem",
+          display: "inline-block",
+          textShadow: "0 0 10px black", // テキストの影を使用して縁取りの効果を模倣
+          marginLeft:"4vw"
+        }}
+      >
+        ♥
+      </p>
+      </span>
+    );
+  }
+
+export default Like;
