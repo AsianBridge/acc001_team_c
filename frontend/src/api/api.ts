2a03b0590c4b2f329194b5ce142fc97de21a73e6
f-yusei <fkys2932@gmail.com>
2024-03-27 13:03:59 +0900

    feat: 複数のAPIを実装

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,46 @@
+import apiClient from "./apiClient"
+
+const getStoreById = async (storeId:string) => {
+  const postData = {
+    "httpMethod": "GET_STORE",
+    "storeId": storeId
+  }
+  const response = await apiClient.post<string>('', postData);
+  return response.data;
+}
+
+const getMyBingoByUserId = async (userId:string) => {
+  const postData = {
+     "httpMethod": "GET_MYBINGO",
+      "userId": userId
+  }
+  const response = await apiClient.post<string>('', postData);
+  return response.data;
+}
+
+type Review = {
+  bingoId: string;
+  userId: string;
+  storeId: string;
+  caption: string;
+  starTaste: number;
+  starAtmosphere: number;
+  starCP: number;
+}
+
+const postReview = async (review:Review) => {
+  const postData = {
+    "httpMethod": "POST_REVIEW",
+    ...review
+  }
+  const response = await apiClient.post<Review>('', postData);
+  return response.data;
+}
+
+const api = {
+  getStoreById,
+  getMyBingoByUserId,
+  postReview
+}
+
+export default api;
