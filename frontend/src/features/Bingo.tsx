19ffa5e5f7b08882ad511eb178fcfe672988bb26
akito-0520 <n.akito.h17@icloud.com>
2024-04-11 21:33:01 +0900

    rename: "Bingoのファイルの変数の一部変更"

--- frontend/src/features/Bingo.tsx
+++ frontend/src/features/Bingo.tsx
@@ -71,7 +71,7 @@ const storeInformation: BingoSquareModalProps[] = [
 ];
 
 const checkBingo = (storeInformation: BingoSquareModalProps[]) => {
-  const lines = [
+  const BingoLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -82,8 +82,8 @@ const checkBingo = (storeInformation: BingoSquareModalProps[]) => {
     [2, 4, 6],
   ];
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < BingoLines.length; i++) {
+    const [a, b, c] = BingoLines[i];
     if ((storeInformation[a].src != undefined) && (storeInformation[b].src != undefined) && (storeInformation[c].src != undefined)) {
       return true;
     }
