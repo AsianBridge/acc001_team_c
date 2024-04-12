618ead12f9ea47f91d6e5a87d7dc45bea748b119
akito-0520 <n.akito.h17@icloud.com>
2024-04-12 16:17:29 +0900

    fix: "Likeボタンのディレクトリ修正"

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { AccountImage, FooterBingoImage, HomeImage } from "./ShowImage";
+import { useState } from "react";
 
 export const HomeButton = () => {
   return (
@@ -45,6 +46,56 @@ export const ReviewButton = () => {
   return <Button href="/Review">確定ボタン</Button>;
 };
 
+export const LikeButton = () => {
+  const [color, setColor] = useState("white");
+  const [timerId, setTimerId] = useState<number | undefined>(undefined);
+  const [likeCounter, setLikeCounter] = useState<number>(0);
+
+  const handleClick = () => {
+    const ResetTimeout = () => {
+      clearTimeout(timerId);
+    };
+
+    const newTimerId = setTimeout(() => {
+      console.log("時間が経過しました。");
+    }, 2000);
+    setTimerId(newTimerId);
+
+    setColor("red"); // 赤に設定
+
+    ResetTimeout();
+
+    setTimeout(() => {
+      setColor("white"); // 1秒後に白に戻す
+      setLikeCounter(likeCounter + 1);
+    }, 100);
+  };
+
+  return (
+    <header className="Like-header">
+      <p
+        style={{
+          fontSize: "2.0rem",
+          display: "inline-block",
+          marginLeft: "4vw",
+        }}
+      >
+        <span className="likeButton" onClick={handleClick}>
+          <span
+            style={{
+              color: color,
+              textShadow: "0 0 10px black",
+              transition: color === "white" ? "color 0.3s" : "none", // 色が白の場合のみアニメーションを適用
+            }}
+          >
+            ♥
+          </span>
+        </span>
+        <span>×{likeCounter}</span>
+      </p>
+    </header>
+  )
+}
 // export const ImageUploader = () => {
 //   const [base64Images, setBase64Images] = useState<string>();
 
