3341ed61b421a4a1e5dd0c2ddee3206b14304740
akito-0520 <n.akito.h17@icloud.com>
2024-04-16 13:03:23 +0900

    fix: "useStateを関数外で宣言していたのでコメントアウトした"

--- frontend/src/store/stateManager.tsx
+++ frontend/src/store/stateManager.tsx
@@ -1,44 +1,44 @@
-import { useBingoState } from "./ReviewOfBingoState";
-import { useUserState } from "./UserState";
+// import { useBingoState } from "./ReviewOfBingoState";
+// import { useUserState } from "./UserState";
 
-const setBingoID = ({ newBingoID }: { newBingoID: string }) => {
-  const { setBingoID } = useBingoState();
+// const setBingoID = ({ newBingoID }: { newBingoID: string }) => {
+//   const { setBingoID } = useBingoState();
 
-  if (typeof newBingoID === "string") {
-    setBingoID(newBingoID);
-  }
-};
+//   if (typeof newBingoID === "string") {
+//     setBingoID(newBingoID);
+//   }
+// };
 
-const getBingoID = () => {
-  const { BingoID } = useBingoState();
-  return BingoID;
-};
+// const getBingoID = () => {
+//   const { BingoID } = useBingoState();
+//   return BingoID;
+// };
 
-const setStoreID = ({ newStoreID }: { newStoreID: string }) => {
-  const { setStoreID } = useBingoState();
-  if (typeof newStoreID === "string") {
-    setStoreID(newStoreID);
-  }
-};
+// const setStoreID = ({ newStoreID }: { newStoreID: string }) => {
+//   const { setStoreID } = useBingoState();
+//   if (typeof newStoreID === "string") {
+//     setStoreID(newStoreID);
+//   }
+// };
 
-const getUserId = () => {
-  const { userID } = useUserState();
-  return userID;
-};
+// const getUserId = () => {
+//   const { userID } = useUserState();
+//   return userID;
+// };
 
-const setUserId = ({ NewID }: { NewID?: string }) => {
-  const { setUserID } = useUserState();
-  if (typeof NewID === "string") {
-    setUserID(NewID);
-  }
-};
+// const setUserId = ({ NewID }: { NewID?: string }) => {
+//   const { setUserID } = useUserState();
+//   if (typeof NewID === "string") {
+//     setUserID(NewID);
+//   }
+// };
 
-const globalState = {
-  setBingoID,
-  getBingoID,
-  setStoreID,
-  getUserId,
-  setUserId,
-};
+// const globalState = {
+//   setBingoID,
+//   getBingoID,
+//   setStoreID,
+//   getUserId,
+//   setUserId,
+// };
 
-export default globalState;
+// export default globalState;
