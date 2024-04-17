c9124a4298371ce8d535d21d937dc2ac08169276
khuvit <khuvia01@gmail.com>
2024-04-17 21:53:48 +0900

    feat: ログイン画面の実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,19 @@
-<<<<<<< Updated upstream
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Amplify } from "aws-amplify";
-import config from "./amplifyconfiguration.json"
-import {  withAuthenticator } from "@aws-amplify/ui-react";
-
-Amplify.configure(config);
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Router />
-      
-      <Footer />
+      <Auth>
+        <Header />
+        <Router />
+        <Footer />
+      </Auth>
     </BrowserRouter>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
