d95c80d4058be9f69bad5b6b9dd692b616db5536
akito-0520 <n.akito.h17@icloud.com>
2024-04-22 11:31:30 +0900

    add: "新しい型の追加"

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -68,7 +68,74 @@ export type CropperModalProps = {
   minZoom: number;
 };
 
+type starAtmospheres = {
+  star_atmosphere_1: string;
+  star_atmosphere_2: string;
+  star_atmosphere_3: string;
+  star_atmosphere_4: string;
+  star_atmosphere_5: string;
+  star_atmosphere_6: string;
+  star_atmosphere_7: string;
+  star_atmosphere_8: string;
+  star_atmosphere_9: string;
+};
+
+type starCpes = {
+  star_cp_1: string;
+  star_cp_2: string;
+  star_cp_3: string;
+  star_cp_4: string;
+  star_cp_5: string;
+  star_cp_6: string;
+  star_cp_7: string;
+  star_cp_8: string;
+  star_cp_9: string;
+};
+
+type starTastes = {
+  star_taste_1: string;
+  star_taste_2: string;
+  star_taste_3: string;
+  star_taste_4: string;
+  star_taste_5: string;
+  star_taste_6: string;
+  star_taste_7: string;
+  star_taste_8: string;
+  star_taste_9: string;
+};
+
 export interface getDoneBingoIdType {
   statusCode: number;
   body: { user_id: string; flag: string; bingo_id: number } | string;
+}
+
+export interface getMyBingoIdType {
+  statusCode: number;
+  body: string;
+}
+
+export interface getMyBingoIdsBodyType {
+  picture1: string;
+  picture2: string;
+  picture3: string;
+  picture4: string;
+  picture5: string;
+  picture6: string;
+  picture7: string;
+  picture8: string;
+  picture9: string;
+}
+
+export interface ReviewInformation {
+  statusCode: number;
+  body: {
+    star_atmosphere_2: string;
+    star_cp: string;
+    star_taste: string;
+    review: string;
+  };
+}
+
+export interface BingoInformation {
+  goodNumber: number;
 }
