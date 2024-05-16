6365691875d49ba63a2fd8895cb0238ff6958d85
akito-0520 <n.akito.h17@icloud.com>
2024-05-16 21:48:58 +0900

    fix: "buttonコンポーネントの二重化を消去"

--- frontend/src/features/ShowModal.tsx
+++ frontend/src/features/ShowModal.tsx
@@ -140,7 +140,8 @@ export const ShowBingoModal = ({
           margin: 0, // マージンなし
         }}
       >
-        <BingoOfProfile bingoInformation={BingoInformation} />
+        {/* <BingoOfProfile bingoInformation={BingoInformation} /> */}
+        開きます
       </button>
 
       {/* モーダルウィンドウでビンゴカードを拡大して表示 */}
@@ -152,3 +153,4 @@ export const ShowBingoModal = ({
     </div>
   );
 };
+
