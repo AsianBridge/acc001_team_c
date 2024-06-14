9e4b378bd422c05fed3541eb510ae42fa8b9b1d0
akito-0520 <n.akito.h17@icloud.com>
2024-06-14 11:56:28 +0900

    format

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { Authenticator } from "@aws-amplify/ui-react";
 import Home from "./pages/Home/Home";
 import MyAccount from "./pages/MyAccount/MyAccount";
@@ -10,41 +10,41 @@ import SignUpForm from "./pages/SignUpForm/SignUpForm";
 const Router = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/MyAccount"
-          element={
-            <Authenticator>
-              <MyAccount />
-            </Authenticator>
-          }
-        />
-        <Route
-          path="/MyBingo"
-          element={
-            <Authenticator>
-              <MyBingo />
-            </Authenticator>
-          }
-        />
-        <Route
-          path="/Review"
-          element={
-            <Authenticator>
-              <Review />
-            </Authenticator>
-          }
-        />
-        <Route
-          path="/CreateBingoTable"
-          element={
-            <Authenticator>
-              <CreateBingoTable />
-            </Authenticator>
-          }
-        />
-        <Route path="/SignUpForm" element={<SignUpForm />} />
-        </Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/MyAccount"
+        element={
+          <Authenticator>
+            <MyAccount />
+          </Authenticator>
+        }
+      />
+      <Route
+        path="/MyBingo"
+        element={
+          <Authenticator>
+            <MyBingo />
+          </Authenticator>
+        }
+      />
+      <Route
+        path="/Review"
+        element={
+          <Authenticator>
+            <Review />
+          </Authenticator>
+        }
+      />
+      <Route
+        path="/CreateBingoTable"
+        element={
+          <Authenticator>
+            <CreateBingoTable />
+          </Authenticator>
+        }
+      />
+      <Route path="/SignUpForm" element={<SignUpForm />} />
+    </Routes>
   );
 };
 
